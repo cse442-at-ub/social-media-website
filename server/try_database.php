b534ed0fc87d0b85a048169b0e29a4f198907a32
Ke Xu <kexu272@gmail.com>
2023-03-01 13:17:47 -0500

    update PHP MySQL database

--- server/try_database.php
+++ server/try_database.php
@@ -20,6 +20,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
+
 // sql to create table
 $users_info = "CREATE TABLE users_info (
     id INT(8) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
@@ -27,14 +28,23 @@ $users_info = "CREATE TABLE users_info (
     lastname VARCHAR(10) NOT NULL, 
     email VARCHAR(20) NOT NULL, 
     password VARCHAR(20), 
-    chat_history INT(8),
+    user_profile_photo_filename VARCHAR(10), 
+    follows INT(8),
+    blog_history_id INT(8),
     reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 )";
 $blog_history = "CREATE TABLE blog_history (
-    id INT(8) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    blog_history_id INT(8) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     blog_title VARCHAR(50) NOT NULL, 
-    blog_body VARCHAR(600) NOT NULL
+    blog_content VARCHAR(600) NOT NULL, 
+    created_at TIME,
+    user_id INT(8)
     )";
+$follows = "CREATE TABLE follows (
+    follows_id INT(8) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+    user_id INT(8)
+    
+)";
 if ($conn->query($users_info) === TRUE) {
     echo "Table users_info created successfully";
 } else {
