739c1c32cc6aa3ea362f193a5ff271b0c999869d
Haoxiang <60672702+Haoxiang-56@users.noreply.github.com>
2023-03-12 12:57:08 -0400

    #27

---
+++ User page/src/App.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='UserPage'>
+        <p>User Page</p>
+        <button type='button' className='MyPage'>
+          <p1>My Page</p1>
+          <br/></button>
+        <button type='button' className='MyFans'>
+          <p2>My Fans</p2>
+          <br/></button>
+        <button type='button' className='MyFollowing'>
+          <p3>My Following</p3>
+          <br/></button>
+        <button type='button' className='MyBlog'>
+          <p6>My Blog</p6>
+          <br/></button>
+        <button type='button' className='MyAlbum'>
+          <p7>My Album</p7>
+          <br/></button>
+      </div>
+      <div className='User'>
+        <button type='button' className='Back'>
+          <p4>Back</p4>
+          <br/></button>
+        <div className='username'>
+          <button type='button' className='Image'>
+            <p5>Image</p5>
+            <br/></button>
+          <p8>Name</p8>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
