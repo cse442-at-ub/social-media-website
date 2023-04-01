f69b8b0713fdd1fb8b6001bd614339a9191cdf27
zhexichen <74996996+MaRonggg@users.noreply.github.com>
2023-04-01 11:27:25 -0400

    able to delete table #39

--- Spring3/database.php
+++ Spring3/database.php
@@ -20,10 +20,13 @@ $sql = "CREATE TABLE user (
     id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(30) NOT NULL,
     userpass VARCHAR(30) NOT NULL,
+    reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 )";
 
-if (mysqli_query($conn, $sql)) {
-    echo "Table created successfully";
-} else {
-    echo "Error creating table: " . mysqli_error($conn);
-}
+if (!mysqli_query($conn, $sql)) {
+    echo "Table created failed";
+} 
+echo "Table created successfully";
+
+// Delete table
+// $sql = "DROP TABLE user";
