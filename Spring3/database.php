f82465d0c825fd3e7e305b1ca96461467ba47679
Ke Xu <kexu272@gmail.com>
2023-04-01 12:22:35 -0400

    update table user_info #39

--- Spring3/database.php
+++ Spring3/database.php
@@ -13,7 +13,7 @@ $conn = mysqli_connect($servername, $username, $password, $dbname);
 if (mysqli_connect_errno()) {
     die("Connection failed: " . mysqli_connect_error());
 } else {
-    echo "Connected";
+    echo "Connected" . "<br>";
 }
 
 // Create table
@@ -25,12 +25,12 @@ if (mysqli_connect_errno()) {
 
 $sql = "CREATE TABLE users_info (
     id INT(8) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    firstname VARCHAR(30) NOT NULL, 
-    lastname VARCHAR(30) NOT NULL, 
-    age VARCHAR(10) NOT NULL,
-    email VARCHAR(20) NOT NULL, 
+    firstname VARCHAR(255) NOT NULL, 
+    lastname VARCHAR(255) NOT NULL, 
+    age VARCHAR(3) NOT NULL,
+    email VARCHAR(255) NOT NULL, 
     password VARCHAR(255) NOT NULL, 
-    user_profile_photo_filename VARCHAR(10) NOT NULL, 
+    user_profile_photo_filename VARCHAR(255) NOT NULL, 
     follows INT(8) NOT NULL,
     fans INT(8) NOT NULL, 
     blog_history_id INT(8) NOT NULL
@@ -38,9 +38,9 @@ $sql = "CREATE TABLE users_info (
 
 
 if (mysqli_query($conn, $sql)) {
-    echo "Table created successfully";
+    echo "Table created successfully" . "<br>";
 } else {
-    echo "Error creating table: " . mysqli_error($conn);
+    echo "Error creating table: " . mysqli_error($conn) . "<br>";
 }
 
 // to Delete table
