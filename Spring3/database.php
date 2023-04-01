6cc43133758ef0d5c8b4ebd9c0acfde249ddd279
Ke Xu <kexu272@gmail.com>
2023-04-01 12:10:15 -0400

    update table user_info and insert #39

--- Spring3/database.php
+++ Spring3/database.php
@@ -12,8 +12,9 @@ $conn = mysqli_connect($servername, $username, $password, $dbname);
 // Check connection
 if (mysqli_connect_errno()) {
     die("Connection failed: " . mysqli_connect_error());
+} else {
+    echo "Connected";
 }
-echo "Connected";
 
 // Create table
 $sql = "CREATE TABLE users (
@@ -22,11 +23,40 @@ $sql = "CREATE TABLE users (
     userpass VARCHAR(30) NOT NULL
 )";
 
-// Delete table
+
+
+
+$sql = "CREATE TABLE users_info (
+    id INT(8) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    firstname VARCHAR(10) NOT NULL, 
+    lastname VARCHAR(10) NOT NULL, 
+    email VARCHAR(20) NOT NULL, 
+    password VARCHAR(255) NOT NULL, 
+    user_profile_photo_filename VARCHAR(10) NOT NULL, 
+    follows INT(8) NOT NULL,
+    fans INT(8) NOT NULL, 
+    blog_history_id INT(8) NOT NULL
+)";
+
+
+
+
+$sql = "INSERT INTO users_info (
+                        id, firstname, lastname, email, password, 
+                        user_profile_photo_filename, follows, fans, 
+                        blog_history_id
+                        ) VALUES (
+                                  1, 'Doe', 'john','john@example.com', 
+                                  'password', 'image1.jpg', 1, 1, 1)";
+
+
+
+
+if (mysqli_query($conn, $sql)) {
+    echo "Table created successfully";
+} else {
+    echo "Error creating table: " . mysqli_error($conn);
+}
+
+// to Delete table
 // $sql = "DROP TABLE user";
-
-
-if (!mysqli_query($conn, $sql)) {
-    echo "Table created failed";
-} 
-echo "Table created successfully";
