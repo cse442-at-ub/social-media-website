c0ab25e0f47f4f50c5d42a21da84932936c8439e
Ke Xu <kexu272@gmail.com>
2023-04-07 01:37:05 -0400

    update handle_Homepage to make post and store to db #42

--- Spring3/handle_Homepage.php
+++ Spring3/handle_Homepage.php
@@ -1,26 +1,61 @@
 <?php
 
 
-header('Access-Control-Allow-Origin: *');
-$mypost = $GLOBALS['HTTP_RAW_POST_DATA'];
-
-
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // collect value of input field
-    $POST_text = $_POST['POST_text'];
-    $POST_image = $_POST['POST_image'];
-    $response = array('status' => 'success',
-        "POST_text" => $POST_text, "POST_image" => $POST_image);
+    $received_data = json_decode(file_get_contents('php://input'), true);
+//    $input_auth = $received_data ["Auth_token"];
+    $input_title = $received_data ["Text"];
+//    if isset($received_data["Image"]) then $received_data["Image"]; otherwise NULL;
+    $input_image = $received_data["Image"] ?? NULL;
+
+
+    $response = array("status" => "success", "Text" => $input_title, "Image" => $input_image);
+
+
+    if (isset($_COOKIE['auth_token'])) {
+        $response['Auth_token'] = $_COOKIE['auth_token'];
+    } else {
+        $response['status'] = 'Auth token not found';
+    }
+
+
+    $servername = "localhost";
+    $username = "root";
+    $db_password = null;
+    $dbname = "spring3_database";
+    // Create connection
+    $conn = mysqli_connect($servername, $username, $db_password, $dbname);
+
+    // retrieve the email by using auth_token
+    $stmt = $conn->prepare("SELECT email FROM users_info WHERE auth_token = ?");
+    $stmt->bind_param('s', $_COOKIE['auth_token']);
+    $stmt->execute();
+    $row = $stmt->get_result()->fetch_assoc();
+    $email = $row['email'];
+
+    // insert row data into post_history
+    $stmt = $conn->prepare(
+        "INSERT INTO post_history (
+                          email, 
+                          title, like_count, like_id, image_id, comments_id
+                          ) VALUES (
+                                    ?, ?, NULL, NULL, ?, NULL)");
+
+    $stmt->bind_param('ssi', $email, $input_title, $input_image);
+    $stmt->execute();
+    if ($stmt->affected_rows <= 0) {
+        $response["status"] = "Error: " . $stmt->error;
+    }
+
+    $stmt->close();
+    $conn->close();
+
+
+
+
+
+
     echo json_encode($response);
-
-    echo "\n HOMEPAGE is connected successfully!";
-    header('Content-Type: application/json');
-
 }
 
-
-$mypost = $GLOBALS['HTTP_RAW_POST_DATA'];
-$postdata = json_decode($mypost);
-$username = $postdata->user_email;
-$password = $postdata->user_password;
 
