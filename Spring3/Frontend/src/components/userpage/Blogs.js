fcbe65a8f4218a817c66726856aab2bef21ed703
Steve li <happystevenli9755@gmail.com>
2023-04-09 00:34:20 -0400

    update userpage blog #48

--- Spring3/Frontend/src/components/userpage/Blogs.js
+++ Spring3/Frontend/src/components/userpage/Blogs.js
@@ -6,29 +6,47 @@ import withAuth from '../../auth.js';
 import axios from 'axios';
 
 
-const checkblogs = async()  => {
-    try {
-       const response = await axios.get('load_personal_posts.php');
-       const data = response.data
+// const checkblogs = async()  => {
+//     try {
+//        const response = await axios.get('load_personal_posts.php');
+//        const data = response.data
+//
+//        if (data.cookie_is_set){
+//            const postTitles = response.data.posts.map(post => post.post_title);
+//            const firstPostFirstName = response.data.posts[0].first_name;
+//            const date_arr = response.data.posts.map(post => post.post_datetime);
+//
+//            console.log(postTitles);
+//
+//        }
+//
+//     }catch (error) {
+//         console.error(error);
+//     }
+// } ;
 
-       if (data.cookie_is_set){
-           const postTitles = response.data.posts.map(post => post.post_title);
-           const firstPostFirstName = response.data.posts[0].first_name;
-           const date_arr = response.data.posts.map(post => post.post_datetime);
-
-           console.log(postTitles);
-
-       }
-
-
-
-    }catch (error) {
-        console.error(error);
-    }
-} ;
 
 
 const Blogs = ({isLoggedIn}) => {
+    const [postTitles, setPostTitles] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const response = await axios.get('load_personal_posts.php');
+                const data = response.data;
+
+                if (data.cookie_is_set){
+                    const postTitles = data.posts.map(post => post.post_title);
+                    setPostTitles(postTitles);
+                }
+            } catch (error) {
+                console.error(error);
+            }
+        }
+        fetchData();
+    }, []);
+
     const leftButtons = ["Home", "Profile", "Post"];
     return (
         <div className="App">
@@ -37,6 +55,12 @@ const Blogs = ({isLoggedIn}) => {
             </div>
             <div className='Blog'>
                 <p15>You did not put any Blogs !</p15>
+                <ul>
+                    {postTitles.map((title, index) => (
+                        <li key={index}>{title}</li>
+                    ))}
+                </ul>
+
 
             </div>
             <div className = 'Right'>
