e5c748de320052889ebd9ce383e8614817637a55
Haoxiang <60672702+Haoxiang-56@users.noreply.github.com>
2023-04-08 23:01:17 -0400

    #48
    fix problem

--- Spring3/Frontend/src/components/userpage/Blogs.js
+++ Spring3/Frontend/src/components/userpage/Blogs.js
@@ -2,11 +2,15 @@ import './Blogs.css'
 import React from 'react';
 import LeftColumn from "../homepage/LeftColumn";
 import RightColumn from "./RightColumn";
+import withAuth from '../../auth.js';
+import axios from 'axios';
 
 
-const Blogs = () => {
+
+
+
+const Blogs = ({isLoggedIn}) => {
     const leftButtons = ["Home", "Profile", "Post"];
-
     return (
         <div className="App">
             <div className='UserPage'>
@@ -24,4 +28,4 @@ const Blogs = () => {
     );
 
 }
-export default Blogs;
+export default withAuth(Blogs);
