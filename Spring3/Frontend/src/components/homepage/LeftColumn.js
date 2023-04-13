683f465ee479ead33ddc69b6b571db71682df3af
Steve li <happystevenli9755@gmail.com>
2023-04-13 11:37:10 -0400

    updated for register date of birth #54

--- Spring3/Frontend/src/components/homepage/LeftColumn.js
+++ Spring3/Frontend/src/components/homepage/LeftColumn.js
@@ -3,12 +3,14 @@ import "./LeftColumn.css";
 import PostModal from "./PostModal";
 import { useNavigate } from "react-router-dom";
 import withAuth from '../../auth.js';
+import axios from "axios";
+import {toast} from "react-toastify";
 
 const LeftColumn = ({ isLoggedIn, userFullName, userEmail, userAge }) => {
     const [isPostModalOpen, setIsPostModalOpen] = useState(false);
     const leftButtons = isLoggedIn
         ? ["Home", "Profile", "Post"]
-        : ["Home", "Profile", "Post", "Login"];
+        : ["Home", "Profile", "Post", "Login", "Logout"];
     const handleProfileClick = () =>
     {
         if (!isLoggedIn) {
@@ -20,6 +22,32 @@ const LeftColumn = ({ isLoggedIn, userFullName, userEmail, userAge }) => {
     const handlePostClick = () => {
         setIsPostModalOpen(true);
     };
+
+    // added for logout
+    const handleLogoutClick= () => {
+        if (!isLoggedIn) {
+            alert("Please log in before log out");
+            return;
+        }
+        // suppose user has already log in, we will handle the logout
+        alert("not implemented");
+        // here I will make a post request for the php to cancel logout
+        // axios.post('handle_logout.php', {
+        //     // do I need to send anything ?
+        //
+        //
+        // })
+        //     .then((response) => {
+        //         console.log("this is the response itself")
+        //         console.log(response);
+        //
+        //     }, (error) => {
+        //         console.log(error);
+        //     });
+    }
+
+
+
     const navigate = useNavigate();
 
     return (
@@ -36,7 +64,9 @@ const LeftColumn = ({ isLoggedIn, userFullName, userEmail, userAge }) => {
                                 button === "Post" ? handlePostClick :
                                     button === "Profile" ? handleProfileClick :
                                         button ==="Home" ? () => navigate("/"):
-                                            button === "Login" ? () => navigate("/login") : undefined
+                                            button === "Login" ? () => navigate("/login"):
+                                                // added for logout button
+                                                button === "Logout" ? ()  => handleLogoutClick : undefined
                             }
                         >
                             {button}
