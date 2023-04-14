989ef6804199362b58fb74e1dc098862b749dd1f
Ke Xu <kexu272@gmail.com>
2023-04-14 16:57:57 -0400

    handle more case for load_profiles #55

--- Spring3/load_profiles.php
+++ Spring3/load_profiles.php
@@ -4,6 +4,8 @@ header('Access-Control-Allow-Origin: *');
 
 if ($_SERVER["REQUEST_METHOD"] == "GET") {
     $received_data = json_decode(file_get_contents('php://input'), true);
+    $input_user_email = $received_data ["user_email"];
+
 
     $response = array("status" => "success");
 //    "user_first_name" => , "user_last_name" =>
@@ -12,7 +14,6 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
         $response['cookie_is_set'] = true;
 
         $auth_token = $_COOKIE['auth_token'];
-        $input_user_email = $received_data ["user_email"];
 
         $servername = "localhost";
         $username = "root";
@@ -31,12 +32,9 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
 
         if ($input_user_email == $curr_user_email){
             $response['self_page'] = true;
-
-
         } else {
             $response['self_page'] = false;
             $curr_user_email = $input_user_email;
-
         }
 
         $sql = "SELECT firstname, lastname, date_of_birth FROM users_info WHERE email = ?";
@@ -51,7 +49,37 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
         $user_date_of_birth = $row['date_of_birth'];
         $user_age = calculate_age($row['date_of_birth']);
 
+        $response['user_email'] = $user_email;
+        $response['user_first_name'] = $user_first_name;
+        $response['user_last_name'] = $user_last_name;
+        $response['user_full_name'] = $user_first_name . ' ' . $user_last_name;
+        $response['user_date_of_birth'] = $user_date_of_birth;
+        $response['user_age'] = $user_age;
 
+
+    } else {
+        $response['status'] = 'Auth token not found';
+        $response['cookie_is_set'] = false;
+
+        $servername = "localhost";
+        $username = "root";
+        $password = null;
+        $dbname = "spring3_database";
+
+        // Create connection
+        $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+        $sql = "SELECT firstname, lastname, date_of_birth FROM users_info WHERE email = ?";
+        $stmt = mysqli_prepare($conn, $sql);
+        $stmt->bind_param('s', $input_user_email);
+        $stmt->execute();
+
+        $row = $stmt->get_result()->fetch_assoc();
+        $user_email = $input_user_email;
+        $user_first_name = $row['firstname'];
+        $user_last_name = $row['lastname'];
+        $user_date_of_birth = $row['date_of_birth'];
+        $user_age = calculate_age($row['date_of_birth']);
 
         $response['user_email'] = $user_email;
         $response['user_first_name'] = $user_first_name;
@@ -60,24 +88,10 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
         $response['user_date_of_birth'] = $user_date_of_birth;
         $response['user_age'] = $user_age;
 
-        $stmt->close();
-        $conn->close();
-
-
-
-
-    } else {
-        $response['status'] = 'Auth token not found';
-        $response['cookie_is_set'] = false;
-
-
-
-
-
-
     }
 
-
+    $stmt->close();
+    $conn->close();
 
     echo json_encode($response);
 }
