e00c9304914e723c6ffdb7bd452d6167c1f6ed8b
zhexichen <74996996+MaRonggg@users.noreply.github.com>
2023-04-14 15:57:07 -0400

    able to store image and send to the frontend #56

--- Spring3/handle_Homepage.php
+++ Spring3/handle_Homepage.php
@@ -1,15 +1,17 @@
 <?php
 
-
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $received_data = json_decode(file_get_contents('php://input'), true);
-//    $input_auth = $received_data ["Auth_token"];
-    $input_title = $received_data ["Text"];
-//    if isset($received_data["Image"]) then $received_data["Image"]; otherwise NULL;
-    $input_image = $received_data["Image"] ?? NULL;
+//     $received_data = json_decode(file_get_contents('php://input'), true);
+// //    $input_auth = $received_data ["Auth_token"];
+//     $input_title = $received_data ["Text"];
+// //    if isset($received_data["Image"]) then $received_data["Image"]; otherwise NULL;
+//     $input_image = $received_data["Image"] ?? NULL;
 
-
-    $response = array("status" => "success", "Text" => $input_title, "Image" => $input_image);
+    $text = $_POST['Text'];
+    
+    $image = $_FILES['Image'];
+    var_dump($_FILES);
+    $response = array("status" => "success", "Text" => $text, "Image" => $input_image);
 
 
     if (isset($_COOKIE['auth_token'])) {
@@ -37,31 +39,51 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $stmt = $conn->prepare(
         "INSERT INTO post_history (
                           email, 
-                          title, like_count, like_id, image_id, comments_id
+                          title, like_count, like_id, image_name, comments_id
                           ) VALUES (
-                                    ?, ?, NULL, NULL, ?, NULL)");
+                                    ?, ?, NULL, NULL, NULL, NULL)");
 
-    // store image (zhexi)
-    if ($input_image ==NULL){
-        $binary = base64_decode($input_image);
-        $last_row = "SELECT * FROM images ORDER BY primary_key DESC LIMIT 1";
-        $result = mysqli_query($conn, $last_row);
-        $row = mysqli_fetch_assoc($result);
-        $id = $row['id'];
-        $path_in_images = 'images/image' . $id . '.jpg';
-        file_put_contents($path_in_images, $binary);
-    }
-
-
-
-
-    $stmt->bind_param('ssi', $email, $input_title, $input_image);
+    $stmt->bind_param('ss', $email, $text);
     $stmt->execute();
     if ($stmt->affected_rows <= 0) {
         $response["status"] = "Error: " . $stmt->error;
     }
+    
+    // store image (zhexi)
+   
+    
 
-    $stmt->close();
+    if ($image['error'] == UPLOAD_ERR_OK) {
+        $last_row = "SELECT * FROM post_history ORDER BY id DESC LIMIT 1";
+        $result = mysqli_query($conn, $last_row);
+        $row = mysqli_fetch_assoc($result);
+        $id = $row['id'];
+        $path_in_images = 'uploads/image'.$id.'.jpg';
+        $image_name = 'image'.$id.'.jpg';
+        if (move_uploaded_file($image['tmp_name'], $path_in_images)){
+            echo ("moved");
+            $stmt = $conn->prepare(
+                "UPDATE post_history
+                SET image_name = ?
+                WHERE id = ?");
+            $stmt->bind_param('si', $image_name, $id);
+            $stmt->execute();
+        }else{
+            echo ("move failed");
+        }
+        // echo ("stored");
+        
+    } else {
+        // echo ("stored failed");
+    }
+
+
+
+
+
+   
+
+    // $stmt->close();
     $conn->close();
 
 
@@ -69,7 +91,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 
 
-    echo json_encode($response);
+    // echo json_encode($response);
 }
 
 
