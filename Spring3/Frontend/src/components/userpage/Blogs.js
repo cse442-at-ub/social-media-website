32d32021d22d17c247fa7f656cd9607bebc0322d
Steve li <happystevenli9755@gmail.com>
2023-04-15 19:18:14 -0400

    updated for blog for different user email button #63

--- Spring3/Frontend/src/components/userpage/Blogs.js
+++ Spring3/Frontend/src/components/userpage/Blogs.js
@@ -11,50 +11,37 @@ import {useParams} from "react-router-dom";
 const Blogs = ({isLoggedIn, userEmail}) => {
     const [postTitles, setPostTitles] = useState([]);
     const { current_user_email } = useParams()
-    const [userData, setUserData] = useState(null);
-
+    // const [userData, setUserData] = useState(null);
     useEffect(() => {
         async function fetchData() {
             try {
-                const response = await axios.get('load_personal_posts.php');
-                const data = response.data;
-                console.log("this is blog response.data")
-                console.log(data)
-
-                if (data.cookie_is_set){
-                    const postTitles = data.posts.map(post => post.post_title);
-                    const postDate = data.posts.map(post => post.post_datetime);
-                    const firstPostName = data.posts.map(post => post.first_name);
-                    const lastPostName = data.posts.map(post => post.last_name);
-                    setPostTitles(data.posts);
-                }
-            } catch (error) {
-                console.error(error);
-            }
-        }
-
-        if (isLoggedIn){
-            console.log("current people is ")
-            console.log(userEmail)
-            console.log("he is view the other user")
-            console.log(current_user_email)
-
-            axios.post('load_profile_posts.php', {
-                user_email: current_user_email
-            })
-                .then((response) => {
-                    console.log("this is the response data from userpage")
+                // check if logged in and then fetch data from user
+                if (isLoggedIn) {
+                    console.log("in blog page now")
+                    console.log("current people is ")
+                    console.log(userEmail)
+                    console.log("he is viewing the other user")
+                    console.log(current_user_email)
+                    const response = await axios.post('load_profile_posts.php', {
+                        user_email: current_user_email
+                    });
+                    console.log("this is the response data from blog")
                     console.log(response.data)
 
-                    setUserData(response.data);
-
-                }, (error) => {
-                    console.log(error);
-                    console.log("userpage failed")
-                });
+                    const post = response.data.post
+                    const postTitles = response.data.posts.map(post => post.post_title);
+                    const postDate = response.data.posts.map(post => post.post_datetime);
+                    const firstPostName = response.data.posts.map(post => post.first_name);
+                    const lastPostName = response.data.posts.map(post => post.last_name);
+                    setPostTitles(response.data.posts);
+                }
+            } catch (error) {
+                console.log(error);
+                console.log("user page failed")
+            }
         }
         fetchData();
-    }, []);
+    }, [current_user_email, isLoggedIn, setPostTitles, userEmail]);
 
 
     return (
@@ -68,15 +55,15 @@ const Blogs = ({isLoggedIn, userEmail}) => {
                     <p15>You did not put any Blogs!</p15>
                 ) : (
                     postTitles.map((post, index) => (
-                            <Post
-                                key={index}
-                                author={`${post.first_name} ${post.last_name}`}
-                                email={post.email}
-                                content={post.post_title}
-                                image={post.post_image}
-                                postDateTime={post.post_datetime}
-                            />
-                        ))
+                        <Post
+                            key={index}
+                            author={`${post.first_name} ${post.last_name}`}
+                            email={post.email}
+                            content={post.post_title}
+                            image={post.post_image}
+                            postDateTime={post.post_datetime}
+                        />
+                    ))
                 )}
 
             </div>
