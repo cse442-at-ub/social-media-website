d02f2225108fad79f30222fde23103e678f8dc89
Yanbin li <happystevenli9755@gmail.com>
2023-04-26 23:36:09 -0400

    trying to show username and profile photo #73

---
+++ Spring4/Frontend/src/components/userpage/Following_post.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import "../homepage/Post.css";
+import {Link} from "react-router-dom"
+
+// this file will help to display the page content in similar format as post.js
+// it will reuse some of code in post.js
+
+const Following_post = ({ author,email}) => {
+
+    // this page will dilay author and emial and
+
+    return (
+        < div className= "post">
+            <div className="post-header">
+                <Link to = {`/userpage/${email}`} className= "post-author-link">
+                    <img
+                        className="post-avatar"
+                        src = {`https://i.pravatar.cc/50?u=${author}`}
+                        alt = {author}
+                    />
+
+                    <div className="post-author-info">
+                        <span className="post-author">{author}</span>
+                    </div>
+
+                </Link>
+
+            </div>
+
+        </div>
+
+
+
+    );
+
+
+
+};
+
+export default Following_post
