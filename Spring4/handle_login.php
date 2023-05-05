b0c9ed778e1aef26764e7d4437ba8c3e54907409
Ke Xu <kexu272@gmail.com>
2023-05-05 17:30:57 -0400

    update set cookie httponly for security #86

--- Spring4/handle_login.php
+++ Spring4/handle_login.php
@@ -38,6 +38,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $cookie_value = bin2hex(random_bytes(16));
 //            echo '$cookie_value: ' . $cookie_value;
             $expiration = time() + (60 * 60 * 24 * 7);
+            $secure = true;
+            $httponly = true;
+            setcookie($cookie_name, $cookie_value, $expiration, $secure, $httponly);
+
             setcookie($cookie_name, $cookie_value, $expiration);
 
 //            update the auth_token in the users_info table
